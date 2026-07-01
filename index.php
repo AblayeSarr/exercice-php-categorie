74b077d5d06a36ee6a7507bf897df540fa14e2ba
Ablaye Sarr <sarrablaye295@gmail.com>
2026-07-01 02:54:28 +0000

    feat: définition des données initiales des catégories

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// 1 Définir les données 
+
+$categories = [
+   0 => [
+        "code" => "a295",
+        "nom" => "categorie0",
+        "produits" => [
+              0 => [
+                "nom" => "produit1",
+                "reference" => "ref1",
+                "prix" => 5000,
+                "quantite" => 5 
+              ],
+              1 => [
+                "nom" => "produit2",
+                "reference" => "ref2",
+                "prix" => 7000,
+                "quantite" => 7 
+              ]
+        ]
+   ],
+   1 => [
+        "code" => "a295",
+        "nom" => "categorie1",
+        "produits" => []
+   ]
+];
