9042cdbe901ca7681e6589961d1095b80a7dc23c
Ablaye Sarr <sarrablaye295@gmail.com>
2026-07-01 02:56:52 +0000

    feat: ajout d'un produit à une catégorie existante

--- index.php
+++ index.php
@@ -77,3 +77,26 @@ $categorie = [
 ];
 
 $categories[] = $categorie;
+
+// 4 Ajouter un produit à une catégorie
+
+$categorieExiste = false;
+$code = readline("saisir le code :");
+foreach ($categories as $index => $categorie) {
+    if (($categorie["code"]) === $code) {
+        $categorieExiste = true;
+        break;
+    }
+} 
+
+if ($categorieExiste) {
+    $produit = [
+        "nom" => readline("saisir le nom : "),
+        "reference" => readline("saisir la reference : "),
+        "prix" => (int)readline("saisir le prix : "),
+        "quantite" => (int)readline("saisir la quantité : ")
+    ];
+    $categories[$index]["produits"][] = $produit;
+} else {
+    echo " désolé , la categorie n'existe pas...";
+}
