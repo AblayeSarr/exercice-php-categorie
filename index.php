a48d84354b3408bb07327d9e812908df56765c6e
Ablaye Sarr <sarrablaye295@gmail.com>
2026-07-01 03:01:52 +0000

    feat(procedural): validation obligatoire et unique

--- index.php
+++ index.php
@@ -54,3 +54,15 @@ function rechercheCategorieParCle(array $categories, string $key, string $value)
     } 
     return false;
 }
+
+function saisieChampObligatoireEtUnique(array $categories, string $smsSaisie, string $smsError, string $key): string {
+    $valueIsValid = true;
+    do {   
+        $value = saisieChaine($smsSaisie);
+        $valueIsValid = champObligatoire($value, $smsError);
+        if($valueIsValid) {     
+            $valueIsValid = rechercheCategorieParCle($categories, $key, $value);
+        }
+    } while (!$valueIsValid);
+    return $value;
+}
